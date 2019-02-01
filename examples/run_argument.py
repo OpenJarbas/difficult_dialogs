406db3714ab5a07995f95d9b3323e7dc6f9b51af
jarbasal <jarbasai@mailfence.com>
2019-02-01 06:26:14 +0000

    examples

--- examples/run_argument.py
+++ examples/run_argument.py
@@ -3,19 +3,36 @@ from os.path import join, dirname
 from difficult_dialogs.arguments import Argument
 from difficult_dialogs.policy import BasePolicy
 
-arg_folder = join(dirname(__file__), "argument_template")
+arg_folder = join(dirname(__file__), "i_think_therefore_i_am")
 arg = Argument(path=arg_folder)
 dialog = BasePolicy(argument=arg)
 
 
+# load a random premise
+assertion = dialog.pick_assertion()
+dialog.current_assertion = assertion
+
+print(assertion)
+print(assertion.sources)
+
+# select a statement from premise
+statement = dialog.pick_next_statement()
+print(statement)
+
+
 # argument manual control
+
+arg_folder = join(dirname(__file__), "argument_template")
+arg = Argument(path=arg_folder)
+dialog = BasePolicy(argument=arg)
 print(dialog.start())
+
 while not dialog.finished:
+    # get a new premise
     assertion = dialog.pick_assertion()
     if assertion:
-        print(assertion)
         for s in assertion.statements:
             print(s)
-        print(assertion.sources)
     else:
+        # no more premises
         print(dialog.end())
