267a87059f57c6f83b41932a25f52c8a9361a22d
jarbasal <jarbasai@mailfence.com>
2019-02-04 03:07:30 +0000

    doc_strings

--- difficult_dialogs/statements.py
+++ difficult_dialogs/statements.py
@@ -1,20 +1,34 @@
 class Statement(object):
     """
+    A text sentence that may be True or False
     """
-    def __init__(self, text):
+    def __init__(self, text, is_true=True):
         """
 
         Args:
-            text:
+            text:  the text for this statement (str)
+            is_true: truth value of this Statement (bool)
         """
         self.text = str(text)
-        self._true = True
+        self._true = is_true
+
+    def from_json(self, json_dict):
+        """
+        set properties from json
+
+        Args:
+            json_dict: json representation (dict)
+
+        :return:
+        """
+        self.text = json_dict.get("text", "")
+        self._true = json_dict.get("is_true", True)
 
     @property
     def as_json(self):
         """
 
-        Returns:
+        Returns: json representation (dict)
 
         """
         return {"text": self.text,
@@ -27,20 +41,20 @@ class Statement(object):
 
     def agree(self):
         """
-
+        set statement to True
         """
         self._true = True
 
     def disagree(self):
         """
-
+        set statement to False
         """
         self._true = False
 
     def __str__(self):
         """
 
-        Returns:
+        Returns: statement text
 
         """
         return self.text
@@ -48,7 +62,7 @@ class Statement(object):
     def __bool__(self):
         """
 
-        Returns:
+        Returns: statement truth value
 
         """
         return self.is_true
